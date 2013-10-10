1455041a420bccdedbfec3842d33343799626a3b
José Manuel Vázquez Castro <go.jose@live.com.mx>
2013-10-10 17:58:47 -0500

    Sumar Argumentos
    version 1 programa nodejs que suma los numeros pasados como argumentos
    al programa y los imprime en la consola

---
+++ SumaArgumentos.js
@@ -0,0 +1,26 @@
+/**
+*	Programa que acepta numeros de entrada 
+*	como argumentos e imprime la suma de
+*	ellos en consola.
+*/
+
+var resultado = 0
+  , args = process.argv
+
+if (args.length == 2)
+	throw "Favor de Ingresar los numeros a sumar como argumentos."
+
+
+//	Suma de todos los argumentos
+args.forEach(function (element, index, array) {
+	if (index > 1)
+			resultado += Number(element)
+	//si a NaN le sumas algo sigue siendo NaN
+})
+
+// NaN === NaN -> false -- si el resultado es NaN 
+// checar si el resultado es un numero
+if (resultado === resultado) 
+	console.log("La suma de los argumentos es = " + resultado)
+else
+	throw "Alguno de los argumentos que ingresaste no es un numero."
